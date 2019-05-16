9faa0b1a8426fd4c7472ab265e27388bc20cce0b
weizman <weizmangal@gmail.com>
2019-05-16 17:23:47 +0300

    fix support in sourcemap to truley match terser
    also, make it possible to pass generated sourcemap
    that was generated after `terser()` plugin was called

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function terser(userOptions = {}) {
 
       // TODO rewrite with object spread after node6 drop
       const normalizedOptions = Object.assign({}, userOptions, {
-        sourceMap: userOptions.sourcemap !== false,
+        sourceMap: userOptions.sourcemap,
         module: outputOptions.format === "es" || outputOptions.format === "esm"
       });
 
@@ -30,6 +30,22 @@ function terser(userOptions = {}) {
         if (normalizedOptions.hasOwnProperty(key)) {
           delete normalizedOptions[key];
         }
+      }
+
+      if (normalizedOptions.sourceMap && normalizedOptions.sourceMap.get_content) {
+        const typeofGetContent = typeof normalizedOptions.sourceMap.get_content;
+        if (typeofGetContent !== 'function') {
+          throw Error(`'get_content' field of sourcemap option must be of type Function (got ${typeofGetContent} instead)`);
+        }
+
+        normalizedOptions.sourceMap.content = normalizedOptions.sourceMap.get_content();
+
+        const typeofContent = typeof normalizedOptions.sourceMap.content;
+        if (typeofContent !== 'string') {
+          throw Error(`'content' field of sourcemap option must be of type Function (got ${typeofContent} instead)`);
+        }
+
+        delete normalizedOptions.sourceMap.get_content;
       }
 
       const serializedOptions = serialize(normalizedOptions);
